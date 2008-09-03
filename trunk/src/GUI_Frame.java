d96d635a31d99feb9506e2c33e3625e29afc3f14
Arno.Becker <Arno.Becker@dc031e8e-fd55-0410-99c8-6f4f49b7b99b>
2008-09-03 13:54:47 +0000

    meh

--- trunk/src/GUI_Frame.java
+++ trunk/src/GUI_Frame.java
@@ -26,17 +26,46 @@ public class GUI_Frame extends javax.swing.JFrame {
 // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
 private void initComponents() {
 
+jPanel1 = new javax.swing.JPanel();
+jLabel1 = new javax.swing.JLabel();
+jScrollPane1 = new javax.swing.JScrollPane();
+
 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/emotboingpn7.gif"))); // NOI18N
+
+javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+jPanel1.setLayout(jPanel1Layout);
+jPanel1Layout.setHorizontalGroup(
+jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addGroup(jPanel1Layout.createSequentialGroup()
+.addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+.addContainerGap(590, Short.MAX_VALUE))
+);
+jPanel1Layout.setVerticalGroup(
+jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addGroup(jPanel1Layout.createSequentialGroup()
+.addContainerGap()
+.addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+.addContainerGap(19, Short.MAX_VALUE))
+);
+
+jScrollPane1.setBackground(new java.awt.Color(255, 255, 255));
+jScrollPane1.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
 
 javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
 getContentPane().setLayout(layout);
 layout.setHorizontalGroup(
 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-.addGap(0, 400, Short.MAX_VALUE)
+.addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+.addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 619, Short.MAX_VALUE)
 );
 layout.setVerticalGroup(
 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-.addGap(0, 300, Short.MAX_VALUE)
+.addGroup(layout.createSequentialGroup()
+.addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+.addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 314, Short.MAX_VALUE))
 );
 
 pack();
@@ -54,6 +83,9 @@ pack();
     }
 
 // Variables declaration - do not modify//GEN-BEGIN:variables
+private javax.swing.JLabel jLabel1;
+private javax.swing.JPanel jPanel1;
+private javax.swing.JScrollPane jScrollPane1;
 // End of variables declaration//GEN-END:variables
 
 }
