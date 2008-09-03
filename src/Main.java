70a566f9b41739f04637ed168e3b1733dfab277c
mistermartin75 <mistermartin75@dc031e8e-fd55-0410-99c8-6f4f49b7b99b>
2008-09-03 13:31:22 +0000

    Jahaaaaa.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import java.net.InetAddress;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -12,8 +15,22 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    throws Exception
+    {
+	InetAddress localhost = InetAddress.getLocalHost();
+	System.out.println(info(localhost));
+	InetAddress[] ip = InetAddress.getAllByName(localhost.getHostName());
+	for (int i=0; i<ip.length; i++)
+	    System.out.println(info(ip[i]));
+    }
+
+    private static String info(InetAddress ia)
+    {
+	return "\n"+ia
+	+ "\nHostName="+ia.getHostName()
+	+ "\nCanonicalHostName="+ia.getCanonicalHostName()
+	+ "\nHostAddress="+ia.getHostAddress();
     }
 
 }
