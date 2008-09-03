d8ecff3d3fd0d959ababf735358d3c997eb7d12b
Arno.Becker <Arno.Becker@dc031e8e-fd55-0410-99c8-6f4f49b7b99b>
2008-09-03 13:45:51 +0000

    4 klassen gebouwd

---
+++ trunk/src/Control.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.event.*;
+
+public class Control implements WindowListener, ActionListener, ItemListener{
+
+    public void windowOpened(WindowEvent arg0) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void windowClosing(WindowEvent arg0) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void windowClosed(WindowEvent arg0) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void windowIconified(WindowEvent arg0) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void windowDeiconified(WindowEvent arg0) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void windowActivated(WindowEvent arg0) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void windowDeactivated(WindowEvent arg0) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void actionPerformed(ActionEvent arg0) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void itemStateChanged(ItemEvent arg0) {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
