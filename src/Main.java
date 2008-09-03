a1be5a7a8d37edf4fd322d65ccb03ca5a277207d
mistermartin75 <mistermartin75@dc031e8e-fd55-0410-99c8-6f4f49b7b99b>
2008-09-03 13:38:18 +0000

    De goede. Commitzzzzzz.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 
-import java.net.InetAddress;
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
@@ -18,19 +20,20 @@ public class Main {
     public static void main(String[] args)
     throws Exception
     {
-	InetAddress localhost = InetAddress.getLocalHost();
-	System.out.println(info(localhost));
-	InetAddress[] ip = InetAddress.getAllByName(localhost.getHostName());
-	for (int i=0; i<ip.length; i++)
-	    System.out.println(info(ip[i]));
-    }
-
-    private static String info(InetAddress ia)
-    {
-	return "\n"+ia
-	+ "\nHostName="+ia.getHostName()
-	+ "\nCanonicalHostName="+ia.getCanonicalHostName()
-	+ "\nHostAddress="+ia.getHostAddress();
+	ServerSocket ss = new ServerSocket(8080, 80, InetAddress.getLocalHost());
+	System.out.println("Accepting: " + ss);
+	Socket s = ss.accept();
+	System.out.println(s);
+	InputStream is = s.getInputStream();
+	int c;
+	
+	while((c = is.read()) != -1)
+	{
+	    System.out.print((char)c);
+	}
+	is.close();
+	s.close();
+	ss.close();
     }
 
 }
