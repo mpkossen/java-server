baefee342dbbbbd234d95e268ee126255831c16e
mistermartin75 <mistermartin75@dc031e8e-fd55-0410-99c8-6f4f49b7b99b>
2008-09-03 13:54:30 +0000

    Moved to Serverssszzzzz.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.io.InputStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 /**
  *
@@ -17,23 +13,9 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    throws Exception
+    public static void main(String[] args) throws Exception
     {
-	ServerSocket ss = new ServerSocket(8080, 80, InetAddress.getLocalHost());
-	System.out.println("Accepting: " + ss);
-	Socket s = ss.accept();
-	System.out.println(s);
-	InputStream is = s.getInputStream();
-	int c;
-	
-	while((c = is.read()) != -1)
-	{
-	    System.out.print((char)c);
-	}
-	is.close();
-	s.close();
-	ss.close();
+	Server s = new Server();
     }
 
 }
--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,31 @@
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.InputStream;
+import java.net.InetAddress;
 
-public class Server {
-
+public class Server 
+{
+    public Server()
+    throws Exception
+    {
+	ServerSocket ss = new ServerSocket(8080, 80, InetAddress.getLocalHost());
+	System.out.println("Accepting: " + ss);
+	Socket s = ss.accept();
+	System.out.println(s);
+	InputStream is = s.getInputStream();
+	int c;
+    
+    	while((c = is.read()) != -1)
+	{
+	    System.out.print((char)c);
+	}
+	is.close();
+	s.close();
+	ss.close();
+    }
 }
