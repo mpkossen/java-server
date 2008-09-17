79578e39734044b2a0b4693cfb35a0963bd0ae2a
mistermartin75 <mistermartin75@dc031e8e-fd55-0410-99c8-6f4f49b7b99b>
2008-09-17 14:16:53 +0000

    ARNO!!

--- src/server/Response.java
+++ src/server/Response.java
@@ -1,9 +1,12 @@
 package server;
 
+import java.io.FileInputStream;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.io.OutputStream;
 
 /**
  *
@@ -26,9 +29,14 @@ public class Response
 	
     }
     
-    public void setBYTES()
-    {
-	
+    private static void setBYTES(FileInputStream fis, OutputStream os) throws Exception {
+        // Construct a 1K buffer to hold bytes on their way to the socket.
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
+        
+        // Copy requested file into the socket's output stream.
+        while((bytes = fis.read(buffer)) != -1 )
+            os.write(buffer, 0, bytes);
     }
     
     public void send()
--- src/server/Servlet.java
+++ src/server/Servlet.java
@@ -1,9 +1,18 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Vector;
 
 /**
  *
@@ -11,13 +20,78 @@ package server;
  */
 public class Servlet 
 {
+    Socket socket;
+    String path;
+    final static String CRLF ="\r\n";
+    final static String HTTPVERSION = "HTTP/1.0";
+    
     public Servlet()
     {
 	
     }
     
-    public void doGET()
+    public void doGET(Socket socket, String path) throws IOException
     {
+	InputStream is = this.socket.getInputStream();
+        DataOutputStream os = new DataOutputStream(this.socket.getOutputStream());
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        
+	Vector<String> clientrequest = new Vector<String>();
+	String s = br.readLine();
+	while(!s.isEmpty()) {
+	    clientrequest.add(s);
+	    System.out.println("\t"+s);
+	    s = br.readLine();
+	}
+	int statusNummer = 400;
+	String statusLine = "HTTP/1.0 1337 Nigger Not Found" + CRLF; 
+        String contentTypeLine = "";
+        String entityBody = "";
+	String filename = "";
 	
+	for(int i=0;i<clientrequest.size();i++) {
+	    if (clientrequest.elementAt(i).contains("GET")) {
+		filename = path+clientrequest.elementAt(i).split(" ")[1];
+		filename = filename.replaceAll("/+", "/");
+		if (filename.endsWith("/")) {
+		    filename += "index.html";
+		}
+		statusNummer = 200;
+		System.out.println("GET request: bestand "+filename);
+		File f = new File(filename);
+		if (!f.exists()) {
+		    statusNummer = 404;
+		}
+	    }
+	    
+	}
+	
+	
+	String requestLine = clientrequest.elementAt(0);
+
+	switch (statusNummer) {
+	    case 200: statusLine = HTTPVERSION+" 200 OK";break;
+	    case 404: statusLine = HTTPVERSION+" 404 File Not Found";break;
+	    default: statusLine = HTTPVERSION+" 400 Bad Request";break;
+	}
+
+        
+	String returncrap = statusLine+contentTypeLine+entityBody;
+        // Send the status line.
+        os.writeBytes(returncrap);
+	System.out.println("Response:\n\t"+statusLine+"\t"+contentTypeLine+"\t"+entityBody);
+        
+        // Send the content type line.
+       // os.writeBytes(contentTypeLine);
+        
+        // Send a blank line to indicate the end of the header lines.
+        os.writeBytes(CRLF);
+        
+        
+        //Close the streams
+        os.close();
+        br.close();
+        socket.close();
     }
 }
